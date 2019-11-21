31e43a77863f4ac64f6fb06fcf9041b58e5af3ef
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-11-20 20:27:19 -0600

    Test script

---
+++ test_sing_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+singularity run \
+--cleanenv \
+--home INPUTS \
+--bind INPUTS:/INPUTS \
+--bind OUTPUTS:/OUTPUTS \
+baxpr-naleg-roi-master-v1.0.0.simg \
+/INPUTS/roi.nii.gz \
+/INPUTS/na.nii.gz \
+/OUTPUTS \
+UNK_PROJ \
+UNK_SUBJ \
+UNK_SESS \
+UNK_SCAN
